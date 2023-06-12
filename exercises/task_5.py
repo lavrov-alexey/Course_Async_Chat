d5ed518c4a46c66dbef4c6ac5810491cd63d0c98
lavrov-alexey <lavrov-box@mail.ru>
2023-06-12 18:30:03 +0300

    Lesson-01. Task-5 updated

--- exercises/task_5.py
+++ exercises/task_5.py
@@ -9,17 +9,24 @@ if __name__ == '__main__':
     import chardet
 
     # кортеж с именем запускаемой утилиты и параметрами к ней
-    ARGS = ('ping', 'gb.ru', '-n', '6')
-    # создаем подпроцесс запуска команды пинг, результат вывода перехватываем
-    gb_ping = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
-    # выводим результат работы подпроцесса
-    for line in gb_ping.stdout:
-        # вариант явного указания кодовой страницы (хардкод, что нехорошо)
-        # print(line.decode(encoding='cp866'), end='')
-        # определяем код. страницу для каждой байтовой строки вывода скрипта
-        detect_res = chardet.detect(line)
-        # получаем словарь с кодировкой, языком и точностью определения
-        # print(detect_res)
-        detect_enc = detect_res['encoding']
-        # выводим строки с определенной утилитой кодировкой
-        print(line.decode(detect_enc), end='')
+    ARGS = ['ping']
+    WEBSITES = ('yandex.ru', 'youtube.ru')
+    for website in WEBSITES:
+        ARGS.append(website)
+        # создаем подпроцесс запуска команды пинг, результат вывода перехватываем
+        gb_ping = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
+
+        # выводим результат работы подпроцесса
+        print(f'\nПИНГУЕМ САЙТЫ ИЗ СПИСКА. ТЕКУЩИЙ: "{website}"')
+        for line in gb_ping.stdout:
+            # вариант явного указания кодовой страницы (хардкод, что нехорошо)
+            # print(line.decode(encoding='cp866'), end='')
+            # определяем код. страницу для каждой байтовой строки вывода скрипта
+            detect_res = chardet.detect(line)
+            # получаем словарь с кодировкой, языком и точностью определения
+            # print(detect_res)
+            detect_enc = detect_res['encoding']
+            # выводим строки с определенной утилитой кодировкой
+            print(line.decode(detect_enc), end='')
+        # удаляем пропингованный сайт и переходим к следующему
+        ARGS.pop()
