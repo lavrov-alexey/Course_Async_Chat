c0c69c0a212762544a410885ec5ba5af6816d3a3
lavrov-alexey <lavrov-box@mail.ru>
2023-06-22 22:39:16 +0300

    Lesson-04. Unittest utils started

---
+++ unit_tests/unittest_utils.py
@@ -0,0 +1,36 @@
+import unittest
+from os import remove
+from os.path import exists
+from common import utils
+
+FILE_UTF_8 = 'test_file_utf-8.tst'
+FILE_CP1251 = 'test_file_cp1251.tst'
+ENC_UTF_8 = 'utf-8'
+ENC_CP1251 = 'windows-1251'
+TEST_STR = 'Тестовый файл в кодировке '
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        with open(FILE_UTF_8, 'w', encoding=ENC_UTF_8) as f:
+            f.write(TEST_STR + ENC_UTF_8)
+
+        with open(FILE_CP1251, 'w', encoding=ENC_CP1251) as f:
+            f.write(TEST_STR + ENC_CP1251)
+
+    def tearDown(self) -> None:
+        if exists(FILE_UTF_8):
+            remove(FILE_UTF_8)
+        if exists(FILE_CP1251):
+            remove(FILE_CP1251)
+
+    def test_detect_encode_utf_8(self):
+        self.assertEqual(utils.detect_encode(FILE_UTF_8), ENC_UTF_8)
+
+    def test_detect_encode_cp1251(self):
+        self.assertEqual(utils.detect_encode(FILE_CP1251), ENC_CP1251)
+
+
+if __name__ == '__main__':
+    unittest.main()
