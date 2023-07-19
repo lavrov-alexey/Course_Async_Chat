50231d275992769d77c336426e8ddb67c764620b
lavrov-alexey <lavrov-box@mail.ru>
2023-07-19 09:44:07 +0300

    Lesson-07. Started

--- launcher.py
+++ launcher.py
@@ -10,7 +10,7 @@ SERV_CMD_RUN = 'python server.py -p 7777 -a localhost'
 CLIENT_CMD_RUN = 'python client.py localhost 7777'
 
 # сюда будем накапливать запускаемые дочерние процессы
-process = []
+processes = []
 while True:
     user_cmd = input(f'Доступные команды:\n'
                      f'q - выход из скрипта\n'
@@ -21,14 +21,16 @@ while True:
         case 'q':
             break
         case 's':
-            process.append(Popen(SERV_CMD_RUN,
-                                 creationflags=CREATE_NEW_CONSOLE))
+            # запускаем процесс сервера и в цикле нужное кол-во клиентов
+            processes.append(Popen(SERV_CMD_RUN,
+                                   creationflags=CREATE_NEW_CONSOLE))
+            listen_count = input(f'- введите нужное кол-во клиентов '))
             for client_idx in range(CLIENTS_CNT):
-                process.append(Popen(CLIENT_CMD_RUN,
-                                     creationflags=CREATE_NEW_CONSOLE))
+                processes.append(Popen(CLIENT_CMD_RUN,
+                                       creationflags=CREATE_NEW_CONSOLE))
         case 'x':
-            while process:
-                process_to_kill = process.pop()
+            while processes:
+                process_to_kill = processes.pop()
                 process_to_kill.kill()
         case _:  # всё остальное
             print(f'Введена недоступная команда: {user_cmd}')
